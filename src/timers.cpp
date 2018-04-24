e663d65f6b777344bf5d352525b972aff4bf9f30
David Grubb <davidanthonygrubb@gmail.com>
2018-04-24 16:42:24 -0500

    Add a class for implementing and managing timers.

---
+++ src/timers.cpp
@@ -0,0 +1,11 @@
+/*
+ * File: Timers.cpp
+ * Author: dgrubb
+ * Date:24/04/2018
+ */
+
+#include "Timers.h"
+
+using namespace std;
+
+
--- src/timers.h
+++ src/timers.h
@@ -1,27 +1,26 @@
 /*
- * File: A3000.h
+ * File: timers.h
  * Author: dgrubb
  * Date:24/04/2018
  */
 
-#ifndef _A3000_H
-#define _A3000_H
+#ifndef _TIMERS_H
+#define _TIMERS_H
 
 #define A3000_SCREEN_WIDTH  800     // pixels
 #define A3000_SCREEN_HEIGHT 600     // pixels
 #define A3000_CPU_FREQUENCY 8000000 // Hz
 #define A3000_RAM_SIZE      2048    // bytes
 
-class A3000 {
+class Timers {
 
 public:
 
     // Constructors and destructors
-    A3000();
-    ~A3000();
+    Timers();
+    ~Timers();
 
     // Methods
-    int Reset();
 
 private:
 
@@ -31,4 +30,4 @@ private:
     
 };
 
-#endif // _A3000_H
+#endif // _TIMERS_H
