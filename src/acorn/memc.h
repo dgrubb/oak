a7a90c4461dffc26f5293d5ba69b43393c41b4eb
dgrubb <davidanthonygrubb@gmail.com>
2019-08-31 16:55:57 -0500

    Add memory map to MEMC
    1) Add defines for regions of system memory space.

--- src/acorn/memc.h
+++ src/acorn/memc.h
@@ -10,6 +10,33 @@
 #include <stdbool.h> /* bool */
 #include <stdint.h>  /* uint32_t */
 
+/* System memory map
+ * See MEMC Datasheet, page 9
+ */
+#define MEMC_MEMMAP_LOGICAL_RAM_START      0x00000000
+#define MEMC_MEMMAP_LOGICAL_RAM_END        0x01FFFFFF
+#define MEMC_MEMMAP_PHYSICAL_RAM_START     0x02000000
+#define MEMC_MEMMAP_PHYSICAL_RAM_END       0x02FFFFFF
+#define MEMC_MEMMAP_IO_CONTROLLERS_START   0x03000000
+#define MEMC_MEMMAP_IO_CONTROLLERS_END     0x03FFFFFF
+/* VIDC, DMA address generators and address translators
+ * share the same memory address mapping as the ROM. MEMC
+ * delegates access to each depending on whether it's a
+ * read (ROM) or write (everything else) cycle.
+ */
+/* R/W = Read */
+#define MEMC_MEMMAP_ROM_LOW_START          0x03400000
+#define MEMC_MEMMAP_ROM_LOW_END            0x037FFFFF
+#define MEMC_MEMMAP_ROM_HIGH_START         0x03800000
+#define MEMC_MEMMAP_ROM_HIGH_END           0x03FFFFFF
+/* R/W = Write */
+#define MEMC_MEMMAP_VIDEO_CONTROLLER_START 0x03400000
+#define MEMC_MEMMAP_VIDEO_CONTROLLER_END   0x035FFFFF
+#define MEMC_MEMMAP_MEMC_CTRL_DMA_START    0x03600000
+#define MEMC_MEMMAP_MEMC_CTRL_DMA_END      0x037FFFFF
+#define MEMC_MEMMAP_ADDRESS_TRANS_START    0x03800000
+#define MEMC_MEMMAP_ADDRESS_TRANS_END      0x03FFFFFF
+
 /* MEMC interfaces */
 
 /* Inputs */
