cfda5875764d239fdebe9e31d3c83a447653222a
dgrubb <davidanthonygrubb@gmail.com>
2019-08-31 16:38:21 -0500

    Add RESET condition to MEMC
    1) Add placeholder values to MEMC at init/reset.

--- src/acorn/memc.c
+++ src/acorn/memc.c
@@ -19,5 +19,20 @@ memc_init()
 int
 memc_reset()
 {
+    /* TODO: Read the datasheet more thoroughly to figure out
+     * what these should really be on RESET condition. For the time
+     * being just put them to something sensible.
+     */
+    memc_i_a = 0;
+    memc_i_rw = false;
+    memc_i_bw = false;
+    memc_i_seq = false;
+    memc_i_spvmd = false;
+
+    memc_o_ph1 = false;
+    memc_o_ph2 = false;
+    memc_o_ra = 0;
+    memc_o_romcs = false;
+
     return 0;
 }
