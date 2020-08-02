b90cec755b17f504002a3c55a43b777158e837bf
dgrubb <davidanthonygrubb@gmail.com>
2020-08-01 21:17:02 -0500

    Revisit how op-codes are derived.

--- src/acorn/arm2/arm_op.c
+++ src/acorn/arm2/arm_op.c
@@ -8,101 +8,40 @@
 #include "arm2.h"
 #include "debug.h"
 
-ARM2_Op_Test op_test_table[OP_TABLE_SIZE];
-
-void
-arm2_populate_op_test_table()
-{
-    op_test_table[BRANCH].op = BRANCH;
-    op_test_table[BRANCH].test = arm2_is_branch_op;
-
-    op_test_table[DATA_PROCESSING].op = DATA_PROCESSING;
-    op_test_table[DATA_PROCESSING].test = arm2_is_data_processing_op;
-
-    op_test_table[MULTIPLY].op = MULTIPLY;
-    op_test_table[MULTIPLY].test = arm2_is_multiply_op;
-
-    op_test_table[DATA_TRANSFER].op = DATA_TRANSFER;
-    op_test_table[DATA_TRANSFER].test = arm2_is_single_data_transfer_op;
-
-    op_test_table[BLOCK_DATA_TRANSFER].op = BLOCK_DATA_TRANSFER;
-    op_test_table[BLOCK_DATA_TRANSFER].test = arm2_is_block_transfer_op;
-
-    op_test_table[SOFTWARE_INTERRUPT].op = SOFTWARE_INTERRUPT;
-    op_test_table[SOFTWARE_INTERRUPT].test = arm2_is_software_interrupt_op;
-
-    op_test_table[CO_PRO_DATA_TRANSFER].op = CO_PRO_DATA_TRANSFER;
-    op_test_table[CO_PRO_DATA_TRANSFER].test = arm2_is_co_pro_data_transfer_op;
-
-    op_test_table[CO_PRO_DATA_OP].op = CO_PRO_DATA_OP;
-    op_test_table[CO_PRO_DATA_OP].test = arm2_is_co_pro_data_op_op;
-
-    op_test_table[CO_PRO_REGISTER_TRANSFER].op = CO_PRO_REGISTER_TRANSFER;
-    op_test_table[CO_PRO_REGISTER_TRANSFER].test = arm2_is_co_pro_register_transfer_op;
-}
-
 ARM2_Op
 arm2_parse_op(uint32_t op)
 {
-    int i;
-    for (i=0; i<OP_TABLE_SIZE; i++) {
-        if (op_test_table[i].test(op)) {
-            return op_test_table[i].op;
-        }
+    if (0xffffffff == op) {
+        return NONE;
     }
-    return NONE;
+
+    // Bit 27 is always 1 on
+    if (op & (1 << 27)) {
+        return BRANCH;
+    }
+
+    // Bit 26 is always 1
+    if (op & (1 << 26)) {
+        return DATA_TRANSFER;
+    }
+
+    // Immediate flag is set which isn't supported for multiply so we know
+    // this must be a data processing instruction
+    if (op & (1 << 25)) {
+        return DATA_PROCESSING;
+    }
+
+    // Bit 7 is always a 1 in a multiply
+    if (!(op & (1<<7))) {
+        return DATA_PROCESSING;
+    }
+
+    // Bit 4 is also always a 1 in a multiply
+    if (!(op & (1<<4))) {
+        return DATA_PROCESSING;
+    }
+
+    // ... I guess?
+    return MULTIPLY;
 }
 
-bool
-arm2_is_data_processing_op(uint32_t op)
-{
-    return false;
-}
-
-bool
-arm2_is_multiply_op(uint32_t op)
-{
-    return false;
-}
-
-bool
-arm2_is_single_data_transfer_op(uint32_t op)
-{
-    return false;
-}
-
-bool
-arm2_is_block_transfer_op(uint32_t op)
-{
-    return false;
-}
-
-bool
-arm2_is_branch_op(uint32_t op)
-{
-    return false;
-}
-
-bool
-arm2_is_co_pro_data_transfer_op(uint32_t op)
-{
-    return false;
-}
-
-bool
-arm2_is_co_pro_data_op_op(uint32_t op)
-{
-    return false;
-}
-
-bool
-arm2_is_co_pro_register_transfer_op(uint32_t op)
-{
-    return false;
-}
-
-bool
-arm2_is_software_interrupt_op(uint32_t op)
-{
-    return false;
-}
