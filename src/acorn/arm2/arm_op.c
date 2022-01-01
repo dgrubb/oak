8ed9733ccb9b5cca9e5c5e31ebab0befdafc17db
dgrubb <davidanthonygrubb@gmail.com>
2022-01-01 10:48:08 -0600

    Update C++ comments to C-style ones. Tsk.

--- src/acorn/arm2/arm_op.c
+++ src/acorn/arm2/arm_op.c
@@ -11,38 +11,39 @@
 ARM2_Op
 arm2_parse_op(uint32_t op)
 {
-    // NOOP
+    /* NOOP */
     if (0xffffffff == op) {
         return NONE;
     }
 
-    // Bit 27 is always 1 on
+    /* Bit 27 is always 1 on */
     if (op & (1 << 27)) {
         return BRANCH;
     }
 
-    // Bit 26 is always 1
+    /* Bit 26 is always 1 */
     if (op & (1 << 26)) {
         return DATA_TRANSFER;
     }
 
-    // Immediate flag is set which isn't supported for multiply so we know
-    // this must be a data processing instruction
+    /* Immediate flag is set which isn't supported for multiply so we know
+     * this must be a data processing instruction
+     */
     if (op & (1 << 25)) {
         return DATA_PROCESSING;
     }
 
-    // Bit 7 is always a 1 in a multiply
+    /* Bit 7 is always a 1 in a multiply */
     if (!(op & (1<<7))) {
         return DATA_PROCESSING;
     }
 
-    // Bit 4 is also always a 1 in a multiply
+    /* Bit 4 is also always a 1 in a multiply */
     if (!(op & (1<<4))) {
         return DATA_PROCESSING;
     }
 
-    // ... I guess?
+    /* ... I guess? */
     return MULTIPLY;
 }
 
