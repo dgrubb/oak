c95ccc7c5100e5f68179d6f0e76fccee9e9f08ce
dgrubb <davidanthonygrubb@gmail.com>
2023-11-11 10:44:07 -0600

    Populate more CPSR unit tests.

--- test/utCpsr.cc
+++ test/utCpsr.cc
@@ -69,3 +69,141 @@ TEST(Cpsr, CanSetModeSVC)
     cpsr.SetMode(Cpsr::Mode::SVC);
     EXPECT_EQ(Cpsr::Mode::SVC, cpsr.GetMode());
 }
+
+TEST(Cpsr, CanSetGetProgramCounter)
+{
+    auto cpsr = Cpsr();
+    cpsr.SetProgramCounter(0x500);
+    EXPECT_EQ(0x500, cpsr.GetProgramCounter());
+}
+
+TEST(Cpsr, CanSetGetClearStatusFlag_FIQ_DISABLE)
+{
+    auto cpsr = Cpsr();
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE));
+
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE, false);
+    EXPECT_FALSE(cpsr.GetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE));
+}
+
+TEST(Cpsr, CanSetGetClearStatusFlag_IRQ_DISABLE)
+{
+    auto cpsr = Cpsr();
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::IRQ_DISABLE, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::IRQ_DISABLE));
+
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::IRQ_DISABLE, false);
+    EXPECT_FALSE(cpsr.GetStatusFlag(Cpsr::StatusFlag::IRQ_DISABLE));
+}
+
+TEST(Cpsr, CanSetGetClearStatusFlag_OVERFLOW)
+{
+    auto cpsr = Cpsr();
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::OVERFLOW, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::OVERFLOW));
+
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::OVERFLOW, false);
+    EXPECT_FALSE(cpsr.GetStatusFlag(Cpsr::StatusFlag::OVERFLOW));
+}
+
+TEST(Cpsr, CanSetGetClearStatusFlag_CARRY)
+{
+    auto cpsr = Cpsr();
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::CARRY, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::CARRY));
+
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::CARRY, false);
+    EXPECT_FALSE(cpsr.GetStatusFlag(Cpsr::StatusFlag::CARRY));
+}
+
+TEST(Cpsr, CanSetGetClearStatusFlag_ZERO)
+{
+    auto cpsr = Cpsr();
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::ZERO, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::ZERO));
+
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::ZERO, false);
+    EXPECT_FALSE(cpsr.GetStatusFlag(Cpsr::StatusFlag::ZERO));
+}
+
+TEST(Cpsr, CanSetGetClearStatusFlag_NEGATIVE)
+{
+    auto cpsr = Cpsr();
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::NEGATIVE, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::NEGATIVE));
+
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::NEGATIVE, false);
+    EXPECT_FALSE(cpsr.GetStatusFlag(Cpsr::StatusFlag::NEGATIVE));
+}
+
+// Because the program counter is embedded in the
+// same register as the status bits it's important
+// to ensure that setting one doesn't affect the
+// other values. These tests ensures the bitwise logic
+// and masks are correct by verifying changing unrelated
+// fields don't impact each other
+
+TEST(Cpsr, ModeAndProgramCounterAreOrthogonal)
+{
+    auto cpsr = Cpsr();
+
+    // Set testable PC state
+    cpsr.SetProgramCounter(0x500);
+    EXPECT_EQ(0x500, cpsr.GetProgramCounter());
+
+    // Changing mode has no impact on PC
+    cpsr.SetMode(Cpsr::Mode::SVC);
+    EXPECT_EQ(Cpsr::Mode::SVC, cpsr.GetMode());
+    EXPECT_EQ(0x500, cpsr.GetProgramCounter());
+
+    // Changing PC has no impact on mode
+    cpsr.SetProgramCounter(0x2000);
+    EXPECT_EQ(Cpsr::Mode::SVC, cpsr.GetMode());
+    EXPECT_EQ(0x2000, cpsr.GetProgramCounter());
+}
+
+TEST(Cpsr, ModeAndStatusFlagsAreOrthogonal)
+{
+    auto cpsr = Cpsr();
+
+    // Set testable mode state
+    cpsr.SetMode(Cpsr::Mode::SVC);
+    EXPECT_EQ(Cpsr::Mode::SVC, cpsr.GetMode());
+
+    // Changing status flag has no impact on mode
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE));
+    EXPECT_EQ(Cpsr::Mode::SVC, cpsr.GetMode());
+
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE, false);
+    EXPECT_FALSE(cpsr.GetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE));
+    EXPECT_EQ(Cpsr::Mode::SVC, cpsr.GetMode());
+
+    // Changing mode has no impact on status flag
+    cpsr.SetMode(Cpsr::Mode::USER);
+    EXPECT_EQ(Cpsr::Mode::USER, cpsr.GetMode());
+    EXPECT_FALSE(cpsr.GetStatusFlag(Cpsr::StatusFlag::FIQ_DISABLE));
+}
+
+TEST(Cpsr, StatusFlagsAndProgramCounterAreOrthogonal)
+{
+    auto cpsr = Cpsr();
+
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::ZERO, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::ZERO));
+
+    // Changing program counter has no impact on status flag
+    cpsr.SetProgramCounter(0x500);
+    EXPECT_EQ(0x500, cpsr.GetProgramCounter());
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::ZERO));
+
+    cpsr.SetProgramCounter(0x900);
+    EXPECT_EQ(0x900, cpsr.GetProgramCounter());
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::ZERO));
+
+    // Changing status flag has no impact on prorgam counter
+    cpsr.SetStatusFlag(Cpsr::StatusFlag::NEGATIVE, true);
+    EXPECT_TRUE(cpsr.GetStatusFlag(Cpsr::StatusFlag::NEGATIVE));
+    EXPECT_EQ(0x900, cpsr.GetProgramCounter());
+}
